f30f9ab8020e403f673dd6a5bd26e83f73faa886
Joaquin_FG <48914838+JoaquinFG@users.noreply.github.com>
2021-06-29 11:44:44 +0100

    getTenCountries (Random)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,25 @@ export class AppComponent {
 
   constructor(private dataService: DataService){
     this.dataService.getData().subscribe(data => {
-      this.posts = data;
+      this.posts = this.getTenCountries(data)
     })
+  }
+
+  getTenCountries(data:any) {
+
+    let countries: any = [];
+    function getRandomArbitrary(min:any, max: any) {
+        return Math.round(Math.random() * (max - min) + min);
+      }
+    for (let i=0; i<10; i++) {
+      var num = getRandomArbitrary(0, data.length);
+      countries.push(data[num]);
+    }
+    return countries
+  }
+
+  clickButton(){
+    alert("Hola")
   }
 
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 export class DataService {
 
   constructor(private httpClient: HttpClient) { 
-    console.log('service working')
+
   }
 
   getData(){
